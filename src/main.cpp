537066dbb598da17a73c8eec1f5e6abfbb7d7096
githubAileen <aileenjlam@gmail.com>
2022-09-26 17:58:20 -0400

    second commit

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,12 @@
 //No other person's work has been used without due acknowledgement. 
 //I have not made my work available to anyone else.
 
+//Global variables
 int iButton = 0; 
 int buttonLED = 1;
 boolean buttonPressed = false;
 int iVal = 0;
  
-  // read the push button input... 
-  
 void setup() { 
   // configure the USB serial monitor 
   Serial.begin(115200); 
@@ -26,21 +25,33 @@ void setup() {
 } 
  
 void loop() { 
+  // read the push button input... 
   iButton = digitalRead(D5); 
+  // read the analog input
   iVal = analogRead(A0);
   
+  // Check if button is pressed
   if (iButton == 0 ){
+    // If pressed toggle boolean buttonPressed
     buttonPressed = buttonPressed ? false : true;
   }
 
+
+  // Check the state of the buttonPressed
   if(buttonPressed){
+    //If button is pressed, toggle the LED light
     buttonLED = buttonLED ? 0 : 1;
+
+    //Check if light is on or off
     if(buttonLED == 1){
+      //If light is on, display dimmer
       analogWrite(D4, iVal);
     } else {
+      //Else, turn off the light
       digitalWrite(D4, 1);
     }
   }
 
+  // Debounce logic ...
   delay(1); 
 } 
